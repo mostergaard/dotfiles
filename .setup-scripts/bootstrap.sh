35e549e92e83e407680550d52215572f5578df77
Morten Østergaard <moni@siteimprove.com>
2023-09-07 23:20:34 +0200

    More work on mac

--- .setup-scripts/bootstrap.sh
+++ .setup-scripts/bootstrap.sh
@@ -5,4 +5,5 @@ source ./brew.sh
 system_type=$(uname -s)
 if [[ "$system_type" = "Darwin" ]]; then
   setup_brew
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
