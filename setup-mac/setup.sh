95a580dc4775616eaf9317ece8445667fddfdf17
Morten Østergaard <moni@siteimprove.com>
2024-01-16 12:23:14 +0100

    Added NPM and NuGet setup to Mac setup

--- setup-mac/setup.sh
+++ setup-mac/setup.sh
@@ -30,12 +30,55 @@ function config_k8s {
     fi	
     echo "$kubeconfig" >> "$kubeconfig_file"
   fi
-} 
+}
+
+function config_npm_and_nuget {
+  nuget_source_github="https://nuget.pkg.github.com/Siteimprove/index.json"
+  npmrc_file="$HOME/.npmrc"
+
+  dotnet nuget list source | grep -q $nuget_source_github
+  found_nuget_source=$?
+
+  if [[ $found_nuget_source == 1 || ! -f "$npmrc_file" ]]; then
+    echo
+    echo "--- Configuring NPM and/or NuGet ---"
+    echo
+    echo "===> Create a GitHub PAT and copy it into the clipboard."
+    echo "===> 1. Go to https://github.com/settings/tokens"
+    echo "===> 2. Click 'Generate new token' and pick 'Generate new token (classic)' and authenticate to GitHub if needed"
+    echo "===> 3. Name the token - e.g. 'GitHub Packages Read on Mac dev machine'"
+    echo "===> 4. Select 'No expiration' and check the 'read:packages' permission"
+    echo "===> 5. Click the 'Generate token' button"
+    echo "===> 6. The token appears in the UI - now click the 'copy' button to copy it to the clipboard"
+    echo "===> 7. For the new PAT, click the 'Configure SSO' dropdown and click 'Authorize' for the Siteimprove org"
+    echo "===> Click 'enter' when done"
+    read
+    pat="$(pbpaste)"
+
+    if [ ! -f "$npmrc_file" ]; then
+      echo "Configuring NPM"
+      echo "@siteimprove:registry=https://npm.pkg.github.com/" >> "$npmrc_file"
+      echo "//npm.pkg.github.com/:_authToken=$pat" >> "$npmrc_file"
+    fi
+
+    if [[ $found_nuget_source == 1 ]]; then
+      echo "Configuring NuGet source for GitHub"
+      dotnet nuget add source --username USERNAME --password $pat --store-password-in-clear-text --name "Siteimprove GitHub" $nuget_source_github
+    fi 
+  fi
+
+  echo "Making sure LTS version of node and npm is installed"
+  volta install node
+
+  echo "Installing yarn as a global tool"
+  volta install yarn
+}
 
 system_type=$(uname -s)
 if [[ "$system_type" = "Darwin" ]]; then
   setup_brew
   setup_oh_my_zsh
+  setup_dotnet
   config_k8s
-  #setup_dotnet
+  config_npm_and_nuget
 fi
